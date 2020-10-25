e1c72ff03820fad2f6099a79ec5e393f17e07091
BruhManInddee <65510750+BruhManInddee@users.noreply.github.com>
2020-10-25 01:17:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,30 @@
 import os
 import discord
 from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.pornhub.com"
 
 token = os.environ.get('token')
-bot = commands.Bot(command_prefix='>')
+bot = commands.Bot(command_prefix='~')
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def vid(ctx):
+    response = requests.get(url)
+    html = response.text
+
+    soup = BeautifulSoup(html, "html.parser")
+    links = soup.findAll("a")
+    for link in links:
+    href = str(link.get("href"))
+    if href.startswith("/view") and href[-1].isdigit():
+      response = requests.get("https://pornhub.com"+href)
+      html = response.text
+      soup = BeautifulSoup(html, "html.parser")
+      links = soup.findAll('title')
+      sentence = str(link)[7:len(link)-9]+" - https://pornhub.com"+str(href)
+      await ctx.send(sentence)
 
 bot.run(token)
 print("on")
