a7282ba6981c571184048ddfd07289e17c56c051
BruhManInddee <65510750+BruhManInddee@users.noreply.github.com>
2020-12-05 19:40:43 +0000

    Update bullybrad.py

--- bullybrad.py
+++ bullybrad.py
@@ -3,14 +3,17 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-token = token = os.environ.get('token')
+token = os.environ.get('token')
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="your mum's booty hole"
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Fuck off </3"))
     
     async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
         args = message.content.split(" ")
         if args[0].lower() != "beeb":
             return
